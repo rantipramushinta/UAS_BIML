b08d3552e2b8526757cd6ed49f1a0d15019d843b
rantipramushinta <97375083+rantipramushinta@users.noreply.github.com>
2022-01-09 13:37:29 +0700

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/House_Price'
-r = requests.post(url,json={'Area(in sq. ft)', 'Price(in Rs.)'})
+r = requests.post(url,json={'Area(in sq. ft)':9, 'Price(in Rs.)':9})
 
 print(r.json())
